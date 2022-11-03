1514d1a1b202aa68abbf999e69f9277a4c7e171c
gabrielagabriela <gabrielaa.slv@gmail.com>
2022-11-03 14:17:43 -0300

    ex_02

---
+++ src/cerveja/cerveja.entity.ts
@@ -0,0 +1,20 @@
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { TipoCerveja } from './tipo-cerveja.enum';
+
+export class Cerveja {
+  @IsNotEmpty()
+  @IsString()
+  nome: string;
+
+  @IsNotEmpty()
+  @IsString()
+  descricao: string;
+
+  @IsNotEmpty()
+  @IsString()
+  nomeCervejaria: string;
+
+  @IsNotEmpty()
+  @IsEnum(TipoCerveja)
+  tipo: TipoCerveja;
+}
